a941ba7377a8a9c9fd2cce9c9f53c0ceb055f3a7
valentina <valentinagiannone@gmail.com>
2020-01-09 15:36:25 +0100

    classe persona ok

--- getOop.php
+++ getOop.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 // GOAL:
 //- Definire la classe Persona caratterizzata da nome e cognome
@@ -6,3 +6,26 @@
 //- Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
 //- Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre
 //- Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
+
+//svolgimento :
+
+// Definire la classe Persona caratterizzata da nome e cognome OK
+class Persona {
+
+    public $nome;
+    public $cognome;
+
+    // generare costruttore che acq. 2 dim.
+    function __construct($nome, $cognome) {
+
+     $this -> nome = $nome;
+     $this -> cognome =  $cognome;
+
+    }
+
+   public function __toString() {
+   /* rappresentazione testuale dell'oggetto */
+     return   $this -> nome . "\n"
+            . $this -> cognome;
+   }
+}
