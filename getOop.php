049682b2dbbd6c9d27dd116294e835bb848fc16f
valentina <valentinagiannone@gmail.com>
2020-01-09 16:05:06 +0100

    problema a riga 51 e 71

--- getOop.php
+++ getOop.php
@@ -30,6 +30,54 @@ class Persona {
    }
 }
 
+//- Definire la classe Ospite (che eredita da Persona) caratterizzata da nome,
+//cognome e anno di nascita
+class Ospite extends Persona {
 
-$pers = new Persona (Frida, Khalo);
-echo $pers ;
+    public $annoDnascita;
+
+  //- Per ogni classe definire costruttore e toString in maniera appropriata,
+  //eventualmente richiamando i metodi della classe padre
+  function __construct($nome, $cognome , $annoDnascita) {
+
+    parent:: __construct($nome, $cognome);
+    $this -> annoDnascita = $annoDnascita;
+
+  }
+
+  }
+  function __toString() {
+
+    return parent:: __toString() $this -> annoDnascita;
+
+  }
+}
+
+//- Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
+class Pagante extends Persona {
+
+    public $residenza;
+
+  function __construct($nome, $cognome , $residenza) {
+    parent:: __construct($nome, $cognome);
+    $this -> residenza = $residenza;
+
+  }
+
+  }
+  
+  function __toString() {
+
+    return parent:: __toString() $this -> residenza;
+
+  }
+}
+
+ //- Eseguire dei test, istanziando ogni classe definita e testando la
+ //correttezza dei risultati attesi
+ $pers = new Persona (Frida, Khalo);
+ echo $pers . "\n". "\n";
+ $osp = new Ospite (Frida, Khalo, 1907);
+ echo $osp "\n". "\n";
+ $res = new Pagante (Frida, Khalo, 1907);
+ echo $res "\n". "\n";
