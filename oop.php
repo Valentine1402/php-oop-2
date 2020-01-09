d9b2f7b105a562692a36fbf3e4ac4d6c53bae1b8
valentina <valentinagiannone@gmail.com>
2020-01-09 15:06:35 +0100

    creazione file

---
+++ oop.php
@@ -0,0 +1,8 @@
+<?php  
+
+// GOAL:
+//- Definire la classe Persona caratterizzata da nome e cognome
+//- Definire la classe Ospite (che eredita da Persona) caratterizzata da nome, cognome e anno di nascita
+//- Definire la classe Pagante (che eredita da Persona) caratterizzata da nome, cognome e indirizzo (di residenza)
+//- Per ogni classe definire costruttore e toString in maniera appropriata, eventualmente richiamando i metodi della classe padre
+//- Eseguire dei test, istanziando ogni classe definita e testando la correttezza dei risultati attesi
